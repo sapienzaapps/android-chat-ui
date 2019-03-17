4b99d7f7b07a7e1859d4e0de1edc44ef39f9a7fc
Daniel Hrituc <hrituc.1693268@studenti.uniroma1.it>
2019-03-17 21:19:07 +0100

    ChatMessage.TYPE implements Serializable

--- chat-ui/src/main/java/co/intentservice/chatui/models/ChatMessage.java
+++ chat-ui/src/main/java/co/intentservice/chatui/models/ChatMessage.java
@@ -73,7 +73,7 @@ public class ChatMessage implements Serializable {
         this.sender = sender;
     }
 
-    public enum Type {
+    public enum Type implements Serializable {
         SENT, RECEIVED
     }
 }
